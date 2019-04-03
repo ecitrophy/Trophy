41c7527a486b1eccc4da5842842b7cb5898da9fc
JuanJoseAndrade <juan.andrade96@hotmail.com>
2019-04-03 03:20:47 -0500

    add user

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withStyles, Grid, TextField, Button} from '@material-ui/core';
 import { Face, Lock, Email } from '@material-ui/icons';
 import { withRouter } from 'react-router-dom';
+import axios from "axios";
 const styles = theme => ({
     margin: {
         margin: theme.spacing.unit * 2,
@@ -14,8 +15,55 @@ const styles = theme => ({
 class RegisterTab extends React.Component {
   constructor(props) {
       super(props);
-      localStorage.setItem("accessToken","" );
+      this.state = {email: "", password: "",name: "",username: ""};
+      localStorage.setItem("accessToken", "");
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleEmailChange = this.handleEmailChange.bind(this);
+      this.handlePasswordChange = this.handlePasswordChange.bind(this);
+      this.handleNameChange = this.handleNameChange.bind(this);
+      this.handleUserNameChange = this.handleUserNameChange.bind(this);
   }
+  handleEmailChange(e) {
+          this.setState({
+              email: e.target.value
+          });
+      }
+  handlePasswordChange(e) {
+          this.setState({
+              password: e.target.value
+          });
+      }
+  handleNameChange(e) {
+          this.setState({
+              name: e.target.value
+          });
+      }
+  handleUserNameChange(e) {
+          this.setState({
+              username: e.target.value
+          });
+      }
+  handleSubmit() {
+
+
+      axios.post("http://localhost:8080/user/register",
+          {
+              username: this.state.username,
+              name: this.state.name,
+              email: this.state.email,
+              password: this.state.password
+
+
+          }).then((response) => {
+              alert("User created");
+              this.props.history.push('/');
+      }).catch(function (error) {
+          console.log(error);
+          alert(error);
+
+      });
+  }
+
     renderLogin= () =>{
                this.props.history.push('/');
               };
@@ -29,15 +77,25 @@ class RegisterTab extends React.Component {
                         <Face />
                     </Grid>
                     <Grid item md={true} sm={true} xs={true}>
-                        <TextField id="username" label="Username" type="text" fullWidth autoFocus required />
+                        <TextField onChange={this.handleUserNameChange} value={this.state.username} id="username" label="Username" type="text" fullWidth autoFocus required />
                     </Grid>
                 </Grid>
+
+                <Grid container spacing={8} alignItems="flex-end">
+                    <Grid item>
+                        <Face />
+                    </Grid>
+                    <Grid item md={true} sm={true} xs={true}>
+                        <TextField onChange={this.handleNameChange} value={this.state.name}  label="Name" type="text" fullWidth autoFocus required />
+                    </Grid>
+                </Grid>
+
                 <Grid container spacing={8} alignItems="flex-end">
                     <Grid item>
                         <Email />
                     </Grid>
                     <Grid item md={true} sm={true} xs={true}>
-                        <TextField id="email" label="Email" type="email" fullWidth required />
+                        <TextField onChange={this.handleEmailChange} value={this.state.email} id="email" label="Email" type="email" fullWidth required />
                     </Grid>
                 </Grid>
                 <Grid container spacing={8} alignItems="flex-end">
@@ -45,7 +103,7 @@ class RegisterTab extends React.Component {
                         <Lock />
                     </Grid>
                     <Grid item md={true} sm={true} xs={true}>
-                        <TextField id="password" label="Password" type="password" fullWidth required />
+                        <TextField onChange={this.handlePasswordChange} value={this.state.password} id="password" label="Password" type="password" fullWidth required />
                     </Grid>
                 </Grid>
                 <Grid container spacing={8} alignItems="flex-end">
@@ -58,7 +116,7 @@ class RegisterTab extends React.Component {
                 </Grid>
 
                 <Grid container justify="center" style={{ marginTop: '10px' }}>
-                    <Button onClick={this.renderLogin} variant="outlined" color="primary" style={{ textTransform: "none", maxWidth: '400px', minWidth: '400px'}}>Sign In</Button>
+                    <Button onClick={this.handleSubmit} variant="outlined" color="primary" style={{ textTransform: "none", maxWidth: '400px', minWidth: '400px'}}>Sign In</Button>
                 </Grid>
             </div>
         );
