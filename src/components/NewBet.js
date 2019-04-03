99a8b8bbeb93fefc53fabe0f8d95497fb17a7c8e
JuanNicolasGomez <jngm_20@hotmail.com>
2019-04-02 22:37:12 -0500

    post request link to API changed

--- src/components/NewBet.js
+++ src/components/NewBet.js
@@ -71,7 +71,7 @@ class NewBet extends React.Component {
   handleSubmit(e){
       var json = {"name":this.state.name,"creator":"juan.gomez345","bettors":{"user1":15,"user2":0,"user5":15,"user3":15,"user4":15},"state":"WaitingForBets","winner":null,"id":1};
         console.log(JSON.stringify(json));
-      fetch('http://localhost:8080/matcheslist', {
+      fetch('https://gentle-wave-71675.herokuapp.com/matcheslist', {
           method: 'POST',
           headers: {
             'Accept': 'application/json',
